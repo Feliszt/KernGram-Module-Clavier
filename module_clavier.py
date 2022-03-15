9bc7049a39dd87ea52bb2c40301f61270d7fe082
Félix <vmmfelix@gmail.com>
2022-03-15 17:35:47 +0100

    Added updated keyboard processing to module.

--- module_clavier.py
+++ module_clavier.py
@@ -6,7 +6,7 @@ sys.path.append("./KernGram-Module-Abstract")
 from module_abstract import *
 
 # keyboard
-import keyboard
+from pynput import keyboard
 import sys
 
 
@@ -18,41 +18,98 @@ class KernGram_Clavier(KernGramModule) :
         KernGramModule.__init__(self)
         logging.info("Module type [{}] with name [{}] starts.".format(self.config["module_type"], self.config["unique_name"]))
 
+        # variables
+        self.key_history_size       = 10
+        self.key_history            = []
+        self.key_pressed            = ''
+        self.key_held               = ''
+        self.key_held_stored         = ''
+        self.keyboard_state         = -1
+        self.keyboard_state_prev    = -1
+
         #
         self.launch_thread(self.keyboard_processing)
+        self.launch_thread(self.keyboard_listener)
         self.start_play()
         KernGramModule.run_network(self)
 
+    # register a key press
+    def on_press(self, key) :
+        self.key_pressed = key
+        if key == keyboard.Key.esc:
+            quit()
+
     ## methods ##
     # get interaction with keyboard
+    def keyboard_listener(self) :
+        with keyboard.Listener(on_press=self.on_press) as listener:
+            listener.join()
+
+    # get interaction with keyboard
     def keyboard_processing(self) :
-        # pingpong removes event triggering when key is up
-        pingpong = True
         while True :
-            recorded = keyboard.read_key()
-            pingpong = not pingpong
+            # create key history list
+            self.key_history.append(self.key_pressed)
+            self.key_pressed = ''
+            if len(self.key_history) > self.key_history_size :
+                self.key_history.pop(0)
 
-            # key is down
-            if pingpong :
-                # if play state and calib is not empty
-                if self.module_state == 0 and self.calib["events"]:
-                    for event in self.calib["events"] :
-                        if recorded == event["specs"][0] :
-                            logging.info("Event [{} - {}] is DOWN".format(event["id"], event["id"]))
-                            self.send_osc_message("/event", True, (self.ID, 'i'), (event["id"], 'i'), ("DOWN", 's'))
+                # check if a single key has been pressed
+                if self.key_history.count(self.key_history[0]) == len(self.key_history) :
+                    if self.key_history[0] == '' :
+                        self.keyboard_state = 0
+                    else :
+                        self.keyboard_state = 2
+                else :
+                    self.keyboard_state = 1
 
-                # if calibration state
-                if self.module_state == 1 :
-                    # save level
-                    self.can_register_event([recorded])
-                    logging.info("Save level [{}] with temp name.".format(recorded))
-                    print(self.calib)
-                    c = """
-                    if recorded in self.calib["events"] :
-                        logging.info("Cannot save level [{}], too close to [{}] @ {}".format(_delta, event["name"], event["specs"][0]))
-                        self.send_osc_message("/invalid_event", False, (self.ID, 'i'), ("Cannot calibrate this event, it already exists.".format(event["id"]), 's'))
-                        """
+            # a single key has been pressed
+            if self.keyboard_state == 2 and self.keyboard_state_prev != 2 :
+                self.key_held = self.key_history[0]
+                if self.key_held != self.key_held_stored :
+                    if self.key_held_stored != '' :
+                        self.on_key_released(self.key_held_stored)
+                    self.on_key_pressed(self.key_held)
+                self.key_held_stored = self.key_held
 
+            # no keys have been pressed
+            if self.keyboard_state == 0 and self.keyboard_state_prev != 0 and self.keyboard_state_prev != -1 :
+                if self.key_held_stored != '' :
+                    self.on_key_released(self.key_held_stored)
+                self.key_held_stored = ''
+
+            #
+            #print(self.key_history)
+            self.keyboard_state_prev = self.keyboard_state
+            time.sleep(0.05)
+
+    # a key has been pressed
+    def on_key_pressed(self, key) :
+        print("[key_pressed]\t{}".format(key))
+
+        # if play state and calib is not empty
+        if self.module_state == 0 and self.calib["events"] :
+            for event in self.calib["events"] :
+                if str(key) == event["specs"][0] :
+                    logging.info("Event [{} - {}] is DOWN".format(event["id"], event["id"]))
+                    self.send_osc_message("/event", False, (self.ID, 'i'), (event["id"], 'i'), ("DOWN", 's'))
+
+        # if calibration state
+        if self.module_state == 1 :
+            # save level
+            self.can_register_event([str(key)])
+            logging.info("Save level [{}] with temp name.".format(key))
+
+    # a key has been released
+    def on_key_released(self, key) :
+        print("[key_released]\t{}".format(key))
+
+        # if play state and calib is not empty
+        if self.module_state == 0 and self.calib["events"] :
+            for event in self.calib["events"] :
+                if str(key) == event["specs"][0] :
+                    logging.info("Event [{} - {}] is UP".format(event["id"], event["id"]))
+                    self.send_osc_message("/event", False, (self.ID, 'i'), (event["id"], 'i'), ("UP", 's'))
 
     # calibration method
     def start_calibration(self, unused_addr, args) :
