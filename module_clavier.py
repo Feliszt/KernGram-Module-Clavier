00ea7812846d3ee672c363404e8034f13f09f4cf
Félix <vmmfelix@gmail.com>
2022-03-24 16:58:19 +0100

    Included script change to main module script.

--- module_clavier.py
+++ module_clavier.py
@@ -19,38 +19,55 @@ class KernGram_Clavier(KernGramModule) :
         logging.info("Module type [{}] with name [{}] starts.".format(self.config["module_type"], self.config["unique_name"]))
 
         # variables
-        self.key_history_size       = 10
+        self.key_history_size       = 8
+        self.key_check_time         = 0.05
         self.key_history            = []
-        self.key_pressed            = ''
-        self.key_held               = ''
-        self.key_held_stored         = ''
+        self.curr_key_pressed       = ''
+        self.keys_held              = []
         self.keyboard_state         = -1
         self.keyboard_state_prev    = -1
 
         #
-        self.launch_thread(self.keyboard_processing)
-        self.launch_thread(self.keyboard_listener)
+        self.launch_thread(self.keyboard_listening, None)
+        self.launch_thread(self.keyboard_processing, None)
         self.start_play()
         KernGramModule.run_network(self)
 
     # register a key press
     def on_press(self, key) :
-        self.key_pressed = key
+        self.curr_key_pressed = key
         if key == keyboard.Key.esc:
             quit()
 
-    ## methods ##
+    # register a key release
+    def on_release(self, key) :
+        self.launch_thread(self.release_timer, key)
+
+    # a simple timer to delay the processing of a pressed key
+    def release_timer(self, key) :
+        while self.key_history[-1] == key :
+            continue
+        #print("start timer for {}".format(key))
+        for i in range(0, self.key_history_size) :
+            if self.key_history[-1] == key :
+                #print("has {} in key history".format(key))
+                return
+            time.sleep(self.key_check_time)
+        if key in self.keys_held :
+            self.keys_held.remove(key)
+            self.on_key_released(key)
+
     # get interaction with keyboard
-    def keyboard_listener(self) :
-        with keyboard.Listener(on_press=self.on_press) as listener:
+    def keyboard_listening(self) :
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
             listener.join()
 
     # get interaction with keyboard
     def keyboard_processing(self) :
         while True :
             # create key history list
-            self.key_history.append(self.key_pressed)
-            self.key_pressed = ''
+            self.key_history.append(self.curr_key_pressed)
+            self.curr_key_pressed = ''
             if len(self.key_history) > self.key_history_size :
                 self.key_history.pop(0)
 
@@ -65,28 +82,21 @@ class KernGram_Clavier(KernGramModule) :
 
             # a single key has been pressed
             if self.keyboard_state == 2 and self.keyboard_state_prev != 2 :
-                self.key_held = self.key_history[0]
-                if self.key_held != self.key_held_stored :
-                    if self.key_held_stored != '' :
-                        self.on_key_released(self.key_held_stored)
-                    self.on_key_pressed(self.key_held)
-                self.key_held_stored = self.key_held
-
-            # no keys have been pressed
-            if self.keyboard_state == 0 and self.keyboard_state_prev != 0 and self.keyboard_state_prev != -1 :
-                if self.key_held_stored != '' :
-                    self.on_key_released(self.key_held_stored)
-                self.key_held_stored = ''
+                if self.key_history[0] not in self.keys_held :
+                    self.keys_held.append(self.key_history[0])
+                    self.launch_thread(self.on_key_pressed, self.key_history[0])
 
             #
-            #print(self.key_history)
+            #print(threading.active_count())
+            print(self.key_history)
+            #print(self.keys_held)
+            #print(self.keyboard_state)
             self.keyboard_state_prev = self.keyboard_state
-            time.sleep(0.05)
+            time.sleep(self.key_check_time)
 
     # a key has been pressed
     def on_key_pressed(self, key) :
-        print("[key_pressed]\t{}".format(key))
-
+        #print("[key_pressed]\t{}".format(key))
         # if play state and calib is not empty
         if self.module_state == 0 and self.calib["events"] :
             for event in self.calib["events"] :
@@ -100,10 +110,10 @@ class KernGram_Clavier(KernGramModule) :
             self.can_register_event([str(key)])
             logging.info("Save level [{}] with temp name.".format(key))
 
+
     # a key has been released
     def on_key_released(self, key) :
-        print("[key_released]\t{}".format(key))
-
+        #print("[key_released]\t{}".format(key))
         # if play state and calib is not empty
         if self.module_state == 0 and self.calib["events"] :
             for event in self.calib["events"] :
